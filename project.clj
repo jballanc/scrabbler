1886701dd1c1745d422ced91e94c501dff86e5c2
Josh Ballanco <jballanc@gmail.com>
2012-08-20 12:08:04 +0300

    Adapt the algorithm to work with Reducers

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject scrabbler "0.1.0"
-  :description "A simple tool to find the best Scrabble match for a set of letters"
-  :license {:name "BSD 2-Clause License"
-            :url "http://opensource.org/licenses/BSD-2-Clause"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :main scrabbler.core)
+            :description "A simple tool to find the best Scrabble match for a set of letters"
+            :url "https://github.com/jballanc/scrabbler"
+            :license {:name "BSD 2-Clause License"
+                      :url "http://opensource.org/licenses/BSD-2-Clause"}
+            :dependencies [[org.clojure/clojure "1.4.0"]]
+            :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
+            :main scrabbler.core)
--- src/scrabbler/bruteforce.clj
+++ src/scrabbler/bruteforce.clj
@@ -2,23 +2,34 @@
   (:use [clojure.string :only [upper-case]]
         [scrabbler.util :only [points-per-letter dictionary score]]))
 
+(try
+  (require '[clojure.core.reducers :as r])
+  (use '[clojure.core.reducers :only [fold]])
+  (println "Go Reducers!")
+  (catch Throwable _
+    (require '[clojure.core :as r])
+    (def fold reduce)))
+
 (defn possible-with? [letters word]
-  (loop [lett-freq (frequencies (upper-case letters))
-         word      (seq (upper-case word))]
-    (if (empty? word)
-      true
-      (let [next-lett (first word)
-            new-freq (dec (get lett-freq next-lett 0))]
-        (if (< new-freq 0)
-          false
-          (recur (assoc lett-freq next-lett new-freq)
-                 (rest word)))))))
+  (let [letter-freqs (frequencies (upper-case letters))
+        word-lett-freqs (frequencies (seq (upper-case word)))]
+    (letfn [(take-letters [[lett freq]]
+              (- freq (get word-lett-freqs lett 0)))
+            (remaining?
+              ([]
+               true)
+              ([r c]
+               (and r (pos? c))))]
+      (r/reduce remaining? (r/map take-letters letter-freqs)))))
 
 (defn best-match [letters]
-  (reduce (fn [best next]
+  (letfn [(best
+            ([]
+             "")
+            ([best next]
             (if (> (score best) (score next))
               best
-              next))
-          (filter (fn [word]
-                    (possible-with? letters word))
-                  dictionary)))
+              next)))
+          (possible? [word]
+            (possible-with? letters word))]
+  (fold best (r/filter possible? (dictionary)))))
